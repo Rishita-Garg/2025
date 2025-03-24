1de2b588189e0a5160f1cd1d895374f9d1fcb263
Rishita-Garg <125175733+Rishita-Garg@users.noreply.github.com>
2025-03-24 10:09:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./App.css"
+
+// Pages
+import HomePage from "./pages/HomePage"
+import JobListingsPage from "./pages/JobListingsPage"
+import JobDetailsPage from "./pages/JobDetailsPage"
+import JobApplicationPage from "./pages/JobApplicationPage"
+import ApplicationSuccessPage from "./pages/ApplicationSuccessPage"
+import LoginPage from "./pages/LoginPage"
+import RegisterPage from "./pages/RegisterPage"
+import JobSeekerDashboard from "./pages/JobSeekerDashboard"
+import EmployerDashboard from "./pages/EmployerDashboard"
+import PostJobPage from "./pages/PostJobPage"
+
+// Context
+import { AuthProvider } from "./context/AuthContext"
+import PrivateRoute from "./components/PrivateRoute"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <AuthProvider>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/jobs" element={<JobListingsPage />} />
+            <Route path="/jobs/:id" element={<JobDetailsPage />} />
+            <Route
+              path="/jobs/:id/apply"
+              element={
+                <PrivateRoute userType="jobseeker">
+                  <JobApplicationPage />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/jobs/:id/apply/success"
+              element={
+                <PrivateRoute userType="jobseeker">
+                  <ApplicationSuccessPage />
+                </PrivateRoute>
+              }
+            />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route
+              path="/dashboard"
+              element={
+                <PrivateRoute userType="jobseeker">
+                  <JobSeekerDashboard />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/employer/dashboard"
+              element={
+                <PrivateRoute userType="employer">
+                  <EmployerDashboard />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/employer/post-job"
+              element={
+                <PrivateRoute userType="employer">
+                  <PostJobPage />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
+        </div>
+      </Router>
+    </AuthProvider>
+  )
 }
 
-export default App;
+export default App
+
